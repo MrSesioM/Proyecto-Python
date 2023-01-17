9e4bddc85c8416cbe6cd51055aed229a96b55769
antonio sanchez <tony64kx.me@gmail.com>
2023-01-17 19:20:42 +0100

    proyecto

--- Listado.py
+++ Listado.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-
-    
-
-# Se inicializa la variable que almacenará los 11 valores
-valores = []
-
-# Se utiliza un bucle for para solicitar los 11 valores al usuario
-for i in range(11):
-    try:
-        # Se solicita el valor al usuario y se agrega a la lista de valores
-        valor = int(input("Introduzca el número {}: ".format(i+1))) 
-        valores.append(valor)
-
-    # Si el usuario introduce un dato no numérico, se muestra un mensaje de error y se vuelve a solicitar el dato    
-    except ValueError: 
-        print("Error, debe introducir un número entero.") 
-
-        # Se decrementa el contador para volver a solicitar el mismo número al usuario 
-        i -= 1  
-
-    # Al final del bucle for, la lista 'valores' contendrá los 11 valores introducidos por el usuario.
-=======
 import Gestiones
 
 
