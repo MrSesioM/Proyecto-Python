5ae3d521a1fd81873c9789113413f3a8a86a0532
Jesus Marroquin <mchjesus6@gmail.com>
2023-01-18 08:34:07 +0100

    Actualizo Listado.py

--- Listado.py
+++ Listado.py
@@ -10,7 +10,7 @@ def listado_juegos_genero():
 
 def listado_editores():
     lista_editores = []
-    for juego in lista:
+    for juego in Gestiones.lista:
         if (juego[5] not in lista_editores):
             lista_editores.append(juego[5])
     return lista_editores
@@ -18,7 +18,7 @@ def listado_editores():
 
 def listado_plataformas():
     juegos_plataforma = []
-    for juego in lista:
+    for juego in Gestiones.lista:
         if(juego[4] == "Platform"):
             juegos_plataforma.append(juego)
     return juegos_plataforma
