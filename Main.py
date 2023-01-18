280b5344fb74cfa99ef2bef3710bd72c6d8d72d2
Moises <moisesms10@hotmail.com>
2023-01-18 10:56:58 +0100

    Cambio en el enunciado del filtrado de género

--- Main.py
+++ Main.py
@@ -32,7 +32,7 @@ def menu():
             
             case "b":
                 while True:
-                    print("\n1 - Listar todos los juegos.\n2 - Listar todos los editores.\n3 - Listar todos los juegos del género 'plataforma'.\n4 - Listar todos los juegos del siglo XX.\n5 - Listar todos los juegos publicados en años pares.\n6 - Listar todos los juego de Nintendo.\n7 - Listar todos los juegos con mayor media de ventas.\n8 - Listar los 5 juegos mas vendidos de NA, Europa, Japón y del resto del mundo.\n0 - Salir.")
+                    print("\n1 - Listar todos los juegos.\n2 - Listar todos los editores.\n3 - Listar todos los juegos filtrados por género.\n4 - Listar todos los juegos del siglo XX.\n5 - Listar todos los juegos publicados en años pares.\n6 - Listar todos los juego de Nintendo.\n7 - Listar todos los juegos con mayor media de ventas.\n8 - Listar los 5 juegos mas vendidos de NA, Europa, Japón y del resto del mundo.\n0 - Salir.")
                     opcion = input("Introduce una opción: ")
                     
                     match opcion:
