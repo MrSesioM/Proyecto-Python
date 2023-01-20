f92dc395e6a02f6f4deda5e0426245c2543056ab
Alvaro <varo_ccx@hotmail.com>
2023-01-20 01:39:12 +0100

    prueba unitaria

--- _tests/_ttest_introduce_datos.py
+++ _tests/_ttest_introduce_datos.py
@@ -2,8 +2,8 @@ import unittest, Gestiones, random
 
 variable = input("\nSe generaran 11 valores en una lista (numero o texto dependiendo de la posicion en la lista), indica cuantas listas quieres comprobar en la funcion Introduce_datos()\n\n>")
 letras = "abcdefghijklmnopqrstuvwxyz" 
-palabra = random.choice(letras) + random.choice(letras) + random.choice(letras) + random.choice(letras) + random.choice(letras) + random.choice(letras)
 lista = []
+lista_juego=[]
 
 class Test_Utils(unittest.TestCase):
 
@@ -12,19 +12,16 @@ class Test_Utils(unittest.TestCase):
         for contador in range(int(variable)):  
             for i in range(11):
                 
-                if i==1 or i==3 or i==5 or i==6:
+                if (i==0 or i==3 or i>=6):
+                    lista.append(random.randint(1900,2050))
+                    lista_juego.append(Gestiones.lista[contador][i])
+                else:
                     lista.append(random.choice(letras) + random.choice(letras) + random.choice(letras) + random.choice(letras) + random.choice(letras) + random.choice(letras))
-                if i==0:
-                    lista.append(int(random.randint(1900,2050)))
-                elif i==4:
-                    lista.append(int(random.randint(1900,2050)))
-                    lista.append(int(random.randint(1900,2050)))
-                elif i>=7:
-                    lista.append(random.randint(0,10))
-
-            print("Probando lista -->" ,lista)         
+                    lista_juego.append(Gestiones.lista[contador][i])
             
-            self.assertEqual(Gestiones.lista[contador], lista)
+                    print("Probando lista Nº:",contador,"-->" ,lista)         
+            
+            self.assertEqual(lista_juego, lista)
 
 
 if __name__ == '__main__':
